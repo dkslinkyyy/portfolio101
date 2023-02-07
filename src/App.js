5e029c364d29f6afd692616db6c645c061328351
dkslinkyyy <slinky.sthlm@gmail.com>
2023-02-07 13:36:00 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome</h1>
+
+        <ul>
+          <li>About</li>
+          <li>Projects</li>
+          <li>Contact</li>
+
+        </ul>
+      </header>
+    </div>
+  );
+}
+
+export default App;
